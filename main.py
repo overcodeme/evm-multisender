58b810c8c7ff294fe9a7f6a5e5fec744f2b2a0cc
overcodeme <placmabadei1985@rambler.ru>
2025-05-13 17:33:22 +0300

    Chains menu added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 from utils.file_manager import load_txt
 from utils.menu import menu
+from utils.logger import logger
 
 
 WALLETS = load_txt('data/wallets.txt')
--- utils/menu.py
+++ utils/menu.py
@@ -1,19 +1,39 @@
 from colorama import Fore, Style
+from utils.file_manager import load_json
+import os
 
+
+CHAINS = load_json('data/chains.json')
 
 def menu():
+    ch, tok = None
     while True:
+        os.system('cls')
         print(f'{Fore.BLUE}[1]{Style.RESET_ALL} TESTNET')
         print(f'{Fore.BLUE}[2]{Style.RESET_ALL} MAINNET')
+        print(f'{Fore.BLUE}[3]{Style.RESET_ALL} EXIT')
 
         try:
             opt = int(input('>>> '))
-
+            os.system('cls')
             if opt == 1:
-                print('You chose TESTNET')
+                testnet_menu = [*CHAINS["TESTNET"], 'BACK']
+
+                for idx, chain in enumerate(testnet_menu, start=1):
+                    print(f'{Fore.BLUE}[{idx}]{Style.RESET_ALL} {chain}')
+
+
             elif opt == 2:
-                print('You chose MAINNET')
+                mainnet_menu = [*CHAINS['MAINNET'], 'BACK']
+
+                for idx, chain in enumerate(mainnet_menu, start=1):
+                    print(f'{Fore.BLUE}[{idx}]{Style.RESET_ALL} {chain}')
+
+                tok = int(input(">>> "))
+            elif opt == 3:
+                break
             else:
-                print('Wrong option')
+                print(f'{Fore.RED}You picked wrong option{Style.RESET_ALL}')
+
         except Exception as e:
             print('You must input a number')
